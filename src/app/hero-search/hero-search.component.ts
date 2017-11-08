865e0ceabb851536e59cd9ceb64e943afd0d2b0d
davi <davicoradini@gmail.com>
2017-11-08 10:30:01 -0200

    http requests

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class HeroSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
